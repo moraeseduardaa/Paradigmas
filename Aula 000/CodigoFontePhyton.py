1e7a4837b0c7913cdf81df43fff794e9b73e492e
Eduarda Moraes <168236415+moraeseduardaa@users.noreply.github.com>
2026-08-05 21:58:23 -0300

    Update CodigoFontePhyton.py

--- Aula 000/CodigoFontePhyton.py
+++ Aula 000/CodigoFontePhyton.py
@@ -1,5 +1,9 @@
+#solicita o numero e o input aguarda resposta
 numero = int(input("Digite um numero: "))
 
+#laco de repeticao para exibir a tabuada
 for i in range(1,11):
-    resultado = numero * 1
+    #calcula a tabuada conforme o numero digitado    
+    resultado = numero * i
+    #exibe o resultado
     print(f"{numero} x {i} = {numero * i}")
